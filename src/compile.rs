101393b9cc7eea2c9fdb0c00f0a74568e20c8768
wade-cheng <wadecheng3301@gmail.com>
2025-11-08 21:51:40 -0500

    refactor: split work into phases with enum CompileOutput

--- src/compile.rs
+++ src/compile.rs
@@ -3,7 +3,7 @@
 use anyhow::{Context, Result, anyhow};
 use std::ffi::OsStr;
 use std::fs;
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
 use std::process::{Command, Stdio};
 use walkdir::WalkDir;
 
@@ -20,6 +20,77 @@ pub fn source_files(config: &Config) -> impl Iterator<Item = PathBuf> {
         .filter_map(|e| e.ok())
         .filter(|entry| entry.metadata().unwrap().is_file())
         .map(|entry| entry.path().to_path_buf())
+}
+
+pub enum CompileOutput {
+    Noop,
+    Passthrough(PathBuf),
+    RecompileAll,
+    CompileToPath(PathBuf),
+}
+
+impl CompileOutput {
+    pub fn from_full_path(full_path: &Path, config: &Config) -> Result<Self> {
+        if config.passthrough_copy_globs.is_match(full_path) {
+            let rel_path = full_path
+                .strip_prefix(&config.project_root)?
+                .strip_prefix(&config.content_root)?;
+            let dst_path = PathBuf::from(&config.project_root)
+                .join(&config.output_root)
+                .join(rel_path);
+            log::trace!(
+                "CompileOutput::from_full_path({:?}, config) computed Passthrough to {:?}",
+                full_path,
+                dst_path
+            );
+            return Ok(Self::Passthrough(dst_path));
+        }
+
+        if full_path.extension() != Some(&OsStr::new("typ")) {
+            log::trace!(
+                "CompileOutput::from_full_path({:?}, config) computed Noop",
+                full_path
+            );
+            return Ok(Self::Noop);
+        }
+
+        if let Ok(_) = full_path.strip_prefix(config.project_root.join(&config.template_root)) {
+            log::trace!(
+                "CompileOutput::from_full_path({:?}, config) computed RecompileAll",
+                full_path
+            );
+            return Ok(Self::RecompileAll);
+        } else if let Ok(path_to_typ_in_src) =
+            full_path.strip_prefix(config.project_root.join(&config.content_root))
+        {
+            let rel_parent = path_to_typ_in_src.parent().unwrap();
+            let parent_dir_in_dst = config
+                .project_root
+                .join(&config.output_root)
+                .join(rel_parent);
+            let file_in_dst =
+                if full_path.file_name().unwrap() == "index.typ" || config.literal_paths {
+                    let mut file_in_dst = parent_dir_in_dst.join(full_path.file_name().unwrap());
+                    file_in_dst.set_extension("html");
+                    file_in_dst
+                } else {
+                    parent_dir_in_dst
+                        .join(full_path.file_stem().unwrap())
+                        .join("index.html")
+                };
+
+            log::trace!(
+                "CompileOutput::from_full_path({:?}, config) computed CompileToPath to {:?}",
+                full_path,
+                file_in_dst
+            );
+            return Ok(Self::CompileToPath(file_in_dst));
+        }
+
+        unreachable!(
+            "Should not be reachable: all files passed into here should be in src or templates..."
+        )
+    }
 }
 
 pub fn compile_from_scratch(config: &Config) -> Result<()> {
@@ -48,56 +119,36 @@ pub fn compile_from_scratch(config: &Config) -> Result<()> {
     Ok(())
 }
 
-pub fn compile_single(path: &PathBuf, config: &Config) -> Result<()> {
+pub fn compile_single(path: &Path, config: &Config) -> Result<()> {
     log::trace!("here1 compiling {}", path.to_str().unwrap());
-    if config.passthrough_copy_globs.is_match(path) {
-        log::trace!("here2");
-        let path_in_src = path
-            .strip_prefix(config.project_root.join(&config.content_root))
-            .unwrap();
-        let rel_path = path_in_src.parent().unwrap();
-        let parent_dir_in_dst = config.project_root.join(&config.output_root).join(rel_path);
-        let file_in_dst = parent_dir_in_dst.join(path.file_name().unwrap());
 
-        fs::create_dir_all(config.project_root.join(&config.output_root).join(rel_path))?;
-        fs::copy(path, &file_in_dst)?;
-
-        log::trace!(
-            "passthroughcopied {} to {}",
-            path.to_str().unwrap(),
-            file_in_dst.to_str().unwrap()
-        );
-    } else if path.extension() == Some(&OsStr::new("typ")) {
-        log::trace!("here3");
-
-        if let Ok(_) = path.strip_prefix(config.project_root.join(&config.template_root)) {
-            compile_from_scratch(&config)?;
+    match CompileOutput::from_full_path(path, config)? {
+        CompileOutput::Noop => (),
+        CompileOutput::RecompileAll => {
+            compile_from_scratch(config)?
             // need to be careful of infinite recursion, compile_everything calls us (compile)
             // should be fine because this code path should only trigger when compiling
             // on the template root.
             //
             // ... what if someone puts their template code in their src folder?
-        } else if let Ok(path_in_src) =
-            path.strip_prefix(config.project_root.join(&config.content_root))
-        {
-            let rel_path = path_in_src.parent().unwrap();
-            let parent_dir_in_dst = config.project_root.join(&config.output_root).join(rel_path);
-            let file_in_dst = if path.file_name().unwrap() == "index.typ" || config.literal_paths {
-                let mut file_in_dst = parent_dir_in_dst.join(path.file_name().unwrap());
-                file_in_dst.set_extension("html");
-                file_in_dst
-            } else {
-                parent_dir_in_dst
-                    .join(path.file_stem().unwrap())
-                    .join("index.html")
-            };
+        }
+        CompileOutput::Passthrough(dst_path) => {
+            fs::create_dir_all(&dst_path.parent().unwrap())?;
 
-            fs::create_dir_all(config.project_root.join(&config.output_root).join(rel_path))
-                .unwrap();
-            let mut child = {
+            fs::copy(path, &dst_path)
+                .context(format!("Failed to write output to {:?}", &dst_path))?;
+
+            log::trace!(
+                "passthroughcopied {} to {}",
+                path.to_str().unwrap(),
+                dst_path.to_str().unwrap()
+            );
+        }
+        CompileOutput::CompileToPath(dst_path) => {
+            let child = {
                 let args = [
                     OsStr::new("c"),
-                    OsStr::new(path),
+                    OsStr::new(&path),
                     OsStr::new("-"),
                     OsStr::new("--features"),
                     OsStr::new("html"),
@@ -113,8 +164,9 @@ pub fn compile_single(path: &PathBuf, config: &Config) -> Result<()> {
                     .spawn()
                     .context(anyhow!("Failed to run Typst compiler. Maybe you don't have it installed? We ran `typst` with args: {:?}", args))?
             };
-            if config.post_processing_typ.len() > 0 {
-                child = Command::new(&config.post_processing_typ[0])
+
+            let child = if config.post_processing_typ.len() > 0 {
+                Command::new(&config.post_processing_typ[0])
                     .args(&config.post_processing_typ[1..])
                     .stdin(child.stdout.unwrap())
                     .stdout(Stdio::piped())
@@ -122,27 +174,26 @@ pub fn compile_single(path: &PathBuf, config: &Config) -> Result<()> {
                     .context(anyhow!(
                         "Failed to post process. We tried to run the command {:?}",
                         &config.post_processing_typ
-                    ))?;
-            }
+                    ))?
+            } else {
+                child
+            };
 
             let output = child
                 .wait_with_output()
                 .context("Waiting for output of typst and post-processing failed.")?;
 
-            fs::create_dir_all(&file_in_dst.parent().ok_or(anyhow!(
-                "Failed trying to create parent directory of {:?}",
-                &file_in_dst
-            ))?)?;
-            fs::write(&file_in_dst, output.stdout)
-                .context(format!("Failed to write output to {:?}", &file_in_dst))?;
+            fs::create_dir_all(&dst_path.parent().unwrap())?;
+            fs::write(&dst_path, output.stdout)
+                .context(format!("Failed to write output to {:?}", &dst_path))?;
 
             log::trace!(
                 "typfile compiled {} to {}",
                 path.to_str().unwrap(),
-                file_in_dst.to_str().unwrap()
+                dst_path.to_str().unwrap()
             );
         }
-    }
+    };
 
     Ok(())
 }
