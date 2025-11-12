0604d8e5e4a66b9471cf0a56c86d5d806450b986
wade-cheng <wadecheng3301@gmail.com>
2025-11-12 10:04:07 -0500

    refactor: decrease indent, guard clause

--- src/entrypoint.rs
+++ src/entrypoint.rs
@@ -21,38 +21,39 @@ pub fn run(config: &Config) -> Result<()> {
         compile::compile_from_scratch(&config)?;
     }
 
-    if config.watch {
-        let (tx, rx) = mpsc::channel::<DebounceEventResult>();
+    if !config.watch {
+        return Ok(());
+    }
 
-        let mut debouncer =
-            notify_debouncer_full::new_debouncer(Duration::from_millis(200), None, tx)?;
+    let (tx, rx) = mpsc::channel::<DebounceEventResult>();
 
-        debouncer.watch(
-            &config.project_root.join(&config.content_root),
-            RecursiveMode::Recursive,
-        )?;
-        debouncer.watch(
-            &config.project_root.join(&config.template_root),
-            RecursiveMode::Recursive,
-        )?;
-        for res in rx {
-            let events = res.map_err(|errs| {
-                for err in errs {
-                    eprintln!("{:?}", err);
-                }
+    let mut debouncer = notify_debouncer_full::new_debouncer(Duration::from_millis(200), None, tx)?;
 
-                anyhow!("File watcher received errors.")
-            })?;
+    debouncer.watch(
+        &config.project_root.join(&config.content_root),
+        RecursiveMode::Recursive,
+    )?;
+    debouncer.watch(
+        &config.project_root.join(&config.template_root),
+        RecursiveMode::Recursive,
+    )?;
+    for res in rx {
+        let events = res.map_err(|errs| {
+            for err in errs {
+                eprintln!("{:?}", err);
+            }
 
-            for event in events {
-                if let EventKind::Create(_) | EventKind::Modify(_) = event.kind {
-                    compile::compile_batch(event.event.paths.clone().into_iter(), &config)?;
+            anyhow!("File watcher received errors.")
+        })?;
 
-                    if event.event.paths.len() == 1 {
-                        log::info!("recompiled path: {:?}", event.event.paths[0]);
-                    } else {
-                        log::info!("recompiled paths: {:?}", event.event.paths);
-                    }
+        for event in events {
+            if let EventKind::Create(_) | EventKind::Modify(_) = event.kind {
+                compile::compile_batch(event.event.paths.clone().into_iter(), &config)?;
+
+                if event.event.paths.len() == 1 {
+                    log::info!("recompiled path: {:?}", event.event.paths[0]);
+                } else {
+                    log::info!("recompiled paths: {:?}", event.event.paths);
                 }
             }
         }
