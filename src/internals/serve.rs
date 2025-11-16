de613bf84be5c27ef5cc5007572d51ec93e0ef84
wade-cheng <wadecheng3301@gmail.com>
2025-11-16 18:08:45 -0500

    feat: wrap up serve feature

--- src/internals/serve.rs
+++ src/internals/serve.rs
@@ -1,12 +1,19 @@
-use std::{sync::mpsc::Receiver, thread};
+// Code is adapted from MIT-licensed https://github.com/leotaku/tower-livereload/tree/master/examples/livehttpd.
 
+use std::{io::ErrorKind, net::SocketAddr, path::PathBuf, sync::mpsc::Receiver, thread};
+
+use anyhow::{Result, anyhow};
 use axum::{Router, http};
+use tokio::net::TcpListener;
 use tower::layer::util::Stack;
 use tower_http::{services::ServeDir, set_header::SetResponseHeaderLayer};
 use tower_livereload::LiveReloadLayer;
 
 // added crates: tokio
 // axuma xum tower tower_http tower_livereload
+
+// not sure if this no cache stuff is needed, Works On My Machine, but might as well keep it in.
+// I don't notice any terrible performance issues or anything.
 
 type Srhl = SetResponseHeaderLayer<http::HeaderValue>;
 
@@ -29,14 +36,14 @@ fn no_cache_layer() -> Stack<Srhl, Stack<Srhl, Srhl>> {
     )
 }
 
-pub async fn serve(reload_rx: Receiver<()>) {
-    if let Err(error) = try_main(reload_rx).await {
-        eprintln!("{}", error);
+pub async fn serve(reload_rx: Receiver<()>, path: PathBuf) {
+    if let Err(error) = try_serve(reload_rx, path).await {
+        eprintln!("{:?}", error);
         std::process::exit(1);
     }
 }
 
-async fn try_main(reload_rx: Receiver<()>) -> Result<(), Box<dyn std::error::Error>> {
+async fn try_serve(reload_rx: Receiver<()>, path: PathBuf) -> Result<()> {
     let livereload = LiveReloadLayer::new();
     let reloader = livereload.reloader();
 
@@ -48,18 +55,42 @@ async fn try_main(reload_rx: Receiver<()>) -> Result<(), Box<dyn std::error::Err
     });
 
     let app = Router::new()
-        .fallback_service(ServeDir::new(
-            "/home/saffron/Documents/projects/compile-typst-site/examples/typst-site-full/_site",
-        ))
+        .fallback_service(ServeDir::new(path))
         .layer(livereload)
         .layer(no_cache_layer());
 
-    let addr: std::net::SocketAddr = ([0, 0, 0, 0], 8010).into();
-    eprintln!("listening on: http://{}/", addr);
+    let mut listener: Option<TcpListener> = None;
+    let mut addr: Option<SocketAddr> = None;
+    let ports = 8000..8050;
+    for port in ports.clone() {
+        let candidate_addr: std::net::SocketAddr = ([0, 0, 0, 0], port).into();
+        match tokio::net::TcpListener::bind(candidate_addr).await {
+            Ok(candidate_listener) => {
+                listener = Some(candidate_listener);
+                addr = Some(candidate_addr);
+                break;
+            }
+            Err(e) => {
+                if e.kind() == ErrorKind::AddrInUse {
+                    continue;
+                } else {
+                    return Err(e.into());
+                }
+            }
+        }
+    }
+
+    let (Some(listener), Some(addr)) = (listener, addr) else {
+        return Err(anyhow!(
+            "Couldn't serve your website locally. We tried binding to ports {:?}",
+            ports
+        ));
+    };
+
+    log::info!("serving your website locally at the link: http://{}/", addr);
 
     // tracing_subscriber::fmt::init(); // uhh apparently someone already called this somewhere?
 
-    let listener = tokio::net::TcpListener::bind(addr).await?;
     axum::serve(listener, app).await?;
 
     Ok(())
