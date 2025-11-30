fdc48b897a9273687c78233fd3b2b6b70f32862f
wade-cheng <wadecheng3301@gmail.com>
2025-11-30 15:48:53 -0500

    feat: allow all logging modules
    If you want to allow various modules, remember the incantation ahead. It works for the target of logs.
    
        SimpleLogger::new()
            .with_level(log_level_filter)
            // .with_level(LevelFilter::Off)
            // .with_module_level("compile_typst_site", log_level_filter)
            // .with_module_level("typst query stderr", log_level_filter)
            // .with_module_level("init command stderr", log_level_filter)
            .init()
            .unwrap();
    
        log::info!(target: "module name", "example thing...");

--- src/internals/logging.rs
+++ src/internals/logging.rs
@@ -5,19 +5,21 @@ use simple_logger::SimpleLogger;
 
 use crate::internals::config::Config;
 
+fn log_level_filter(config: &Config) -> LevelFilter {
+    if config.trace {
+        LevelFilter::Trace
+    } else if config.verbose {
+        LevelFilter::Debug
+    } else {
+        LevelFilter::Info
+    }
+}
+
 pub fn init(config: &Config) {
+    let log_level_filter = log_level_filter(config);
+
     SimpleLogger::new()
-        .with_level(LevelFilter::Off)
-        .with_module_level(
-            "compile_typst_site",
-            if config.trace {
-                LevelFilter::Trace
-            } else if config.verbose {
-                LevelFilter::Debug
-            } else {
-                LevelFilter::Info
-            },
-        )
+        .with_level(log_level_filter)
         .init()
         .unwrap();
 }
